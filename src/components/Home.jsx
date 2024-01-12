e05a780730d5cb09dbbbee03520f07fae48edea6
rohit <rohit@kali.kali>
2024-01-12 20:58:18 +0530

    nothing video update just

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,7 @@ import './css/home.css';
 import { profile } from './new/Images';
 // import VideoPlayer from 'react-video-js-player';
 // import { drumvideo } from './new/Images';
-const videosource = 'https://youtu.be/LQV_j47pjUQ?si=AwpXVHZMra-0slIC'
+const videosource = 'https://auto-system.onrender.com/files/drumvideo.mp4'
 
 
 
@@ -34,20 +34,21 @@ export default function Home() {
             </div>
           </div>
         </div>
+        <ReactPlayer
+        className="react-player"
+        url={videosource}
+        playing={true}
+        // loop={true}
+        muted={true}
+        // controls={true}
+      />
       </div>
       <div className="wrapper">
         <img src={profile} alt="its me" className="img" />
       </div>
       <hr />
       </div>
-      <ReactPlayer
-        className="react-player"
-        url={videosource}
-        // playing={true}
-        loop={true}
-        muted={true}
-        // controls={true}
-      />
+     
       {/* <div className="react-player">
         <img src={profile} alt="LALALALA" />
       </div> */}
