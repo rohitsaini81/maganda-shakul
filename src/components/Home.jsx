fc8ef19574a2e07a95cfe316f4a077a163768948
rohit <rohit@kali.kali>
2024-01-12 22:44:52 +0530

    color

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import ReactPlayer from 'react-player';
 import './css/home.css';
 import { profile } from './new/Images';
@@ -6,49 +6,61 @@ import { profile } from './new/Images';
 // import { drumvideo } from './new/Images';
 const videosource = 'https://auto-system.onrender.com/files/drumvideo.mp4'
 
-
-
 export default function Home() {
+  const settingdiv = useRef(null);
+  const settingclick = () => {
+    alert("Welcome")
+    settingdiv.current.style.display = 'none';
+  }
+
 
   return (
     <section id='home'>
-    <div className='home'>
-      <div className="overlap">
-        <div className="group-2">
-          <div className="hi-i-am">
-            {/* Hi, I am
+      <div className="settings" ref={settingdiv} onClick={settingclick} style={{
+        position: 'relative',
+         left: '500px', 
+         height: "50px",
+          width: "100px", 
+          backgroundColor: 'red'
+      }}>
+        settings</div>
+      <div className='home'>
+        <div className="overlap">
+          <div className="group-2">
+            <div className="hi-i-am">
+              {/* Hi, I am
             <br /> */}
-            Maganda Shakul 
-          </div>
-          <div className="text-wrapper-5">Musician, Artist, Designer</div>
-          <div className="overlap-group-wrapper">
-            <div className="custom-border-btn overlap-group flex">
-              <a className="btn" href="#">
-                Contact Me....
-              </a>
+              Maganda Shakul
             </div>
-            <div className="div-wrapper flex">
-              <a className="custom-btn" href="#">
-                Learn More
-              </a>
+            <div className="text-wrapper-5">Musician, Artist, Designer</div>
+            <div className="overlap-group-wrapper">
+              <div className="custom-border-btn overlap-group flex">
+                <a className="btn" href="#">
+                  Contact Me....
+                </a>
+              </div>
+              <div className="div-wrapper flex">
+                <a className="custom-btn" href="#">
+                  Learn More
+                </a>
+              </div>
             </div>
           </div>
+          <ReactPlayer
+            className="react-player"
+            url={videosource}
+            playing={true}
+            loop={true}
+            muted={true}
+          // controls={true}
+          />
         </div>
-        <ReactPlayer
-        className="react-player"
-        url={videosource}
-        playing={true}
-        // loop={true}
-        muted={true}
-        // controls={true}
-      />
+        <div className="wrapper">
+          <img src={profile} alt="its me" className="img" />
+        </div>
+        <hr />
       </div>
-      <div className="wrapper">
-        <img src={profile} alt="its me" className="img" />
-      </div>
-      <hr />
-      </div>
-     
+
       {/* <div className="react-player">
         <img src={profile} alt="LALALALA" />
       </div> */}
